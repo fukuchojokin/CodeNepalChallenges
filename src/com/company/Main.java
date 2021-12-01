86f32eb430c7248996772e57ecfd1748226bedff
kshitizbca076 <kshitizbca076@ojashwicollege.edu.np>
2021-12-01 11:48:43 +0545

    this should work

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {1, 3, 6, -2, 8, 1, -9};
+        solution.solution(arr);
+        solution.printList();
+    }
+}
---
+++ src/com/company/Solution.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.*;
+
+public class Solution {
+    List<List<Integer>> list = new ArrayList<>();
+
+    void solution(int[] arr) {
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                for (int k = 0; k < j; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        List<Integer> thisArr = new ArrayList<>();
+                        thisArr.add(arr[i]);
+                        thisArr.add(arr[j]);
+                        thisArr.add(arr[k]);
+                        Collections.sort(thisArr);
+                        list.add(thisArr);
+                    }
+                }
+            }
+        }
+    }
+
+    public <T> List<T> removeDuplicates(List<T> list) {
+        Set<T> set = new LinkedHashSet<>(list);
+        return new ArrayList<>(set);
+    }
+
+    void printList() {
+        list = removeDuplicates(list);
+        list.sort(new Comparator<>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return o1.get(0).compareTo(o2.get(0));
+            }
+        });
+        System.out.println(list);
+    }
+}
